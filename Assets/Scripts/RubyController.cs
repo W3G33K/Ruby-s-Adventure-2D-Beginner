366471eec1d141a8ac1cc3bb381a63a041cc6f82
Ryan K. Clark <mail.w3geek@gmail.com>
2020-03-31 15:12:33 -0400

    Completed 'Character Controller and Keyboard Input' of Ruby's Adventure: 2D Beginner project.
    https://learn.unity.com/tutorial/character-controller-and-keyboard-input?courseId=5c5c1e08edbc2a5465c7ec01&projectId=5c6166dbedbc2a0021b1bc7c

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class RubyController : MonoBehaviour {
 	// Start is called before the first frame update
 	void Start() {
+		// Forces game to run at 10 Frames Per Second.
+		// QualitySettings.vSyncCount = 0;
+		// Application.targetFrameRate = 10;
 	}
 
 	// Update is called once per frame
 	void Update() {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		/*
+		 * If the game runs at ten frames per second, each frame will take 0.1 seconds (1/10).
+		 * Otherwise if it runs at 60 frames per second, each frame will take 0.017 seconds (1/60).
+		 * 
+		 * Debug.Log(Time.deltaTime);
+		 */
+		float deltaTime = Time.deltaTime;
+
 		Vector2 position = transform.position;
-		position.x = position.x + 0.1f;
+		position.x = position.x + 3.0f * horizontal * deltaTime;
+		position.y = position.y + 3.0f * vertical * deltaTime;
 		transform.position = position;
 	}
 }
